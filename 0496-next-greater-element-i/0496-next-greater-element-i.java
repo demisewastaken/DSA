5c3049eca650b084e0cddc41e08cf64e97fab1a9
demisewastaken <dikshitghimire100@gmail.com>
2026-08-22 15:03:48 +0530

    Time: 4 ms (74.17%), Space: 44.9 MB (81.28%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+import java.util.HashMap;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[nums1.length];
+
+        for (int num : nums2) {
+
+            while (!stack.isEmpty() && num > stack.peek()) {
+                map.put(stack.pop(), num);
+            }
+
+            stack.push(num);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return ans;
+    }
+}
